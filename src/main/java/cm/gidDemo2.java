ed236dcf1fb5312ce18a9f3fd1c8717fe9d38c5a
cm <cm_403630@163.com>
2022-07-19 09:43:34 +0800

    master conflict test

--- src/main/java/cm/gidDemo2.java
+++ src/main/java/cm/gidDemo2.java
@@ -9,5 +9,6 @@ public class gidDemo2 {
 //        貌似add项目一次就追踪到项目内容了，之后只需要commit就可
         System.out.println("forth change");
         System.out.println("branch01 merge into master!!!");
+        System.out.println("conflict test...");
     }
 }
