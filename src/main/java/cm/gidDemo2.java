968d0825692c73a04b95f38a241629f3f16a9079
cm <cm_403630@163.com>
2022-07-19 09:31:16 +0800

    gitDemo2 forth commit again！

--- src/main/java/cm/gidDemo2.java
+++ src/main/java/cm/gidDemo2.java
@@ -6,6 +6,7 @@ public class gidDemo2 {
         System.out.println("say hello to the world!!!");
         System.out.println("nothing really matters!");
         System.out.println("third change again");
+//        貌似add项目一次就追踪到项目内容了，之后只需要commit就可
         System.out.println("forth change");
     }
 }
