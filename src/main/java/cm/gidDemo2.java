c515da2cc2896d9d38e83e84282bfdfa73cc799a
cm <cm_403630@163.com>
2022-07-19 09:29:08 +0800

    gitDemo2 forth commit again

--- src/main/java/cm/gidDemo2.java
+++ src/main/java/cm/gidDemo2.java
@@ -6,5 +6,6 @@ public class gidDemo2 {
         System.out.println("say hello to the world!!!");
         System.out.println("nothing really matters!");
         System.out.println("third change again");
+        System.out.println("forth change");
     }
 }
